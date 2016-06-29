a100eb15fb42984f0acad6620702d6ac2812d77d
Jiří Fryč <prosecutor@jirifryc.cz>
2016-06-29 15:04:14 +0200

    Adding token generation

--- src/main/java/prosecutor/barrister/languages/Tokens.java
+++ src/main/java/prosecutor/barrister/languages/Tokens.java
@@ -9,10 +9,45 @@ package prosecutor.barrister.languages;
 ///////////////////////////////////////////////////////////////////////////////
 
 
+import com.sun.org.apache.bcel.internal.generic.RET;
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
+import java.lang.reflect.Field;
+
 public class Tokens {
 
-    public void test()
-    {
+
+    public static void generateIndex(){
+
+        int i=1;
+        try {
+            i=generateIndexFor(Class.class,i);
+            i=generateIndexFor(Conditions.class,i);
+            i=generateIndexFor(Expressions.class,i);
+            i=generateIndexFor(Statements.class,i);
+            i=generateIndexFor(RETURN.class,i);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+    private static int generateIndexFor(java.lang.Class clazz,int startIndex) throws NoSuchFieldException, IllegalAccessException {
+        Field[] fields=clazz.getDeclaredFields();
+        for(int i=0;i<fields.length;i++)
+        {
+            if(fields[i].isAnnotationPresent(Duplicate.class))
+            {
+                Duplicate duplicate=(Duplicate)fields[i].getAnnotation(Duplicate.class);
+                fields[i].setInt(null,duplicate.from().getDeclaredField(duplicate.name().equals("")?fields[i].getName():duplicate.name()).getInt(null));
+
+            }
+            else
+            {
+                fields[i].setInt(null,startIndex++);
+            }
+            return startIndex;
+        }
     }
 
 
